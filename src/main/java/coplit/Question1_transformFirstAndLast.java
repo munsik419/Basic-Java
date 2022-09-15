c18609f0d862b28950de923863df1a2096438f58
kimmunsik <munsik419@gmail.com>
2022-09-15 17:08:43 +0900

    Qusetion1 & Question2

--- src/main/java/coplit/Question1_transformFirstAndLast.java
+++ src/main/java/coplit/Question1_transformFirstAndLast.java
@@ -30,46 +30,43 @@ System.out.println(output); // --> { Kevin : 'Spacey'}
 public class Question1_transformFirstAndLast {
     public static void main(String[] args) {
 
-
-//        String[] arr = new String[100];
-
-//        HashMap<String, String> result = new HashMap<String, String>();
-
         //입력받을 데이터의 개수를 입력받는다
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("입력받을 문자의 개수는?(100개 이하)");
-        int count = sc.nextInt();
+        System.out.println("입력받을 문자의 개수는?");
+        Integer count = sc.nextInt();
+        sc.nextLine();
 
         //배열에 들어갈 문자열을 입력한다
         System.out.println(count + "개의 문자를 입력하세요");
-        String[] arr = new String[100];
+        String[] arr = new String[count];
         for (int i = 0; i < count; ++i) {//
             arr[i] = sc.nextLine();
+
+            //배열에 빈문자열이 입력되면 null을 반환한다
+            if(arr[i].length()==0){
+                arr[i]=null;
+            }
         }
-//        String[] arr = new String[100];
 
         //저장된 배열의 데이터 중에서 첫번째와 마지막 데이터를 key와 value 로 저장한다
         HashMap result = new HashMap();
 
         //배열의 첫번째와 마지막 값을 key와 value로 저장한다
-            result.put(arr[0], arr[arr.length - 1]);
-//        int i = 0;
-//        for (String str : arr) {  //배열 arr의 값을 str 변수에 대입한다
-//            isStringEmpty(true, null); //대입된 값의 길이 index가 0이면
-//            i++;
+        result.put(arr[0], arr[arr.length - 1]);
+
         //저장된 값을 출력한다
-            System.out.println(result);
-        }
+        System.out.println("{" + result.keySet() + " : " + result.values() + "}");
     }
+}
 
-
-//        static boolean isStringEmpty (String arr){
-//            System.out.println("null");
-//            return arr == null;
-//        }
+// 코플릿 풀이 코드
+//    public HashMap<String, String> transformFirstAndLast(String[] arr) {
+//        if(arr.length == 0) return null;
+//        HashMap<String, String> result = new HashMap<>();
+//        result.put(arr[0], arr[arr.length - 1]);
+//        return result;
 //    }
-
 
 
 
--- src/main/java/coplit/Question2_computeWhenDouble.java
+++ src/main/java/coplit/Question2_computeWhenDouble.java
@@ -1,4 +1,59 @@
 package coplit;
 
+import java.util.Scanner;
+
+/*
+문제
+연이율을 입력받아 원금이 2배 이상이 될 때까지 걸리는 시간(년)을 리턴해야 합니다.
+
+입력
+인자 1 : interestRate
+double 타입의 연이율 (%)
+출력
+int 타입을 리턴해야 합니다.
+
+입출력 예시
+int output = computeWhenDouble(7);
+System.out.println(output); // --> 11
+
+output = computeWhenDouble(10);
+System.out.println(output); // --> 8
+*/
 public class Question2_computeWhenDouble {
+    public static void main(String[] args) {
+
+        //연이율을 입력받는다
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("연이율을 입력해 주세요 : ");
+        Double interestRate = sc.nextDouble();
+        Double interestRate1 = interestRate/100;
+
+        //원금을 입력받는다
+        System.out.println("원금을 입력해 주세요 : ");
+        Double amountMoney = sc.nextDouble();
+
+        //원금이 2배 이상이 되는데 걸리는 시간을 구한다
+        int year = 0;
+        Double doubleAmountMoney = amountMoney;
+        while (doubleAmountMoney < amountMoney * 2) {
+            doubleAmountMoney = doubleAmountMoney + (doubleAmountMoney * interestRate1);
+            year++;
+        }
+
+        //원금의 2배가 넘는 시간(년)을 출력한다
+        System.out.println("원금의 2배 이상이 되는 기간은 " + year + " 년 입니다");
+    }
 }
+
+//코플릿 풀이
+//    public int computeWhenDouble(double interestRate) {
+//        double rate = 1 + interestRate / 100;
+//        double principal = 1;
+//        int year = 0;
+//        while (principal < 2) {
+//            principal = principal * rate;
+//            year++;
+//        }
+//        return year;
+//    }
