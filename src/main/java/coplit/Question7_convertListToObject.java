aeb43180cc6eb0ff92e246b83229c23c0f62eb89
kimmunsik <munsik419@gmail.com>
2022-09-27 15:48:44 +0900

    Question7, Question8

---
+++ src/main/java/coplit/Question7_convertListToObject.java
@@ -0,0 +1,64 @@
+package coplit;
+/*
+문제
+2차원 배열(배열을 요소로 갖는 배열)을 입력받아 각 배열을 이용해 만든 HashMap을 리턴해야 합니다.
+
+입력
+인자 1 : arr
+배열을 요소로 갖는 배열
+arr[i]는 String 타입을 요소로 갖는 배열
+arr[i].length는 0 또는 2
+String arr;
+출력
+arr[i]의 첫 번째 요소를 키, 두 번째 요소를 값으로 하는 HashMap<String, String>을 리턴해야 합니다.
+
+주의 사항
+중복되는 키의 경우, 초기의 값을 사용합니다.
+빈 배열을 입력받은 경우, 빈 HashMap을 리턴해야 합니다.
+arr[i]의 길이가 0인 경우, 무시합니다.
+
+입출력 예시
+String[][] arr = new String[]{
+  {'make', 'Ford'},
+  {'model', 'Mustang'},
+  {'year', '1964'},
+  {'make', 'Bill'},
+};
+
+HashMap<String, String> output = convertListToObject(arr);
+
+System.out.println(output) // -->
+{
+  "make" = "Ford"
+  "model" = "Mustang",
+  "year" = "1964"
+}
+
+ */
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Question7_convertListToObject {
+    public static void main(String[] args) {
+        //2차원 배열을 입력받는다
+        Scanner sc = new Scanner(System.in);
+
+        //입력받은 문자열을 2차원배열에 저장한다
+        String[][] arr = new String[2][2];
+
+        for (int i = 0; i < 2; ++i) {
+
+            arr[i][i] = sc.nextLine();sc.nextLine();
+
+            //배열에 빈문자열이 입력되면 null을 반환한다
+            if (arr[i][i].length() == 0) {
+                arr[i][i] = null;
+            }
+        }
+        HashMap result = new HashMap();
+        result.put(arr[0], arr[1]);
+
+        System.out.println(""+result.keySet()+""+"="+result.values()+"");
+    }
+}
---
+++ src/main/java/coplit/Question8_convertDoubleSpaceToSingle.java
@@ -0,0 +1,24 @@
+package coplit;
+/*
+문제
+문자열을 입력받아 해당 문자열에 등장하는 두 칸의 공백을 모두 한 칸의 공백으로 바꾼 문자열을 리턴해야 합니다.
+
+입력
+인자 1 : str
+String 타입의 문자열
+출력
+String 타입의 문자열
+
+주의 사항
+두 칸을 초과하는 공백은 존재하지 않는다고 가정합니다.
+
+입출력 예시
+String output = convertDoubleSpaceToSingle("string  with  double  spaces");
+System.out.println(output); // --> "string with double spaces"
+ */
+
+public class Question8_convertDoubleSpaceToSingle {
+    public static void main(String[] args) {
+
+    }
+}
